453acd822c9ec0b71219cafc001f56b25bbfca1c
Artur Alves <arturalves.ca@gmail.com>
2023-12-01 23:34:59 +0100

    add function to concatenate all sentences from folder

--- custom_utils.py
+++ custom_utils.py
@@ -2,8 +2,49 @@ import json
 import torch
 from pathlib import Path
 from sklearn.model_selection import train_test_split
+from typing import Union, Optional
 
-def gather_dataset(folder_path : str, combine : bool = True) -> tuple[dict, dict, dict]:
+"""
+Concatenates all sentences and *labels in $folder_path to lists
+
+"""
+def read_data(dialogs_folder : str, labels_file : Optional[str]) -> tuple[list, list, Optional[list]]:
+
+    dialogs_path = Path(dialogs_folder)
+    labels_path = Path(labels_file)
+    
+    if not dialogs_path.is_dir():
+        raise Exception(f"Error: {dialogs_folder} is not a folder")
+    if not labels_path.is_file():
+        raise Exception(f"Error: {labels_file} is not a file")
+    
+    sentences = []
+    speakers = []
+    labels = []
+    
+    labels_data = json.load(open(labels_path, "r"))
+    for item in Path(dialogs_path).iterdir():
+        if not item.is_file(): continue 
+        if not item.suffix == ".json": continue
+        
+        # load data
+        dialog = json.load(open(item, "r"))
+        sentences += [exchange["text"] for exchange in dialog]
+        speakers += [exchange["speaker"] for exchange in dialog]
+        
+        if labels_path:
+            labels += labels_data[item.stem]
+    
+    if labels_path:
+        return sentences, speakers, labels
+
+    return sentences, speakers
+
+"""
+Concatenates sentences of each dialog in $folder_path 
+
+"""
+def read_data_by_ID(folder_path : str, combine : bool = True) -> tuple[dict, dict, dict]:
     speakers = dict()
     dialogs = dict()
     edges = dict()
@@ -58,6 +99,7 @@ def tt_split(dialogs, labels, test_size=0.2, random_state=42):
         val_labels += d_val_labels
     
     return train_sentences, val_sentences, train_labels, val_labels
+
 
 def hotencode(X : list) -> list:
     switcher = {
